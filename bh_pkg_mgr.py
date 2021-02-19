2bdb786f6683305da6ee975fe5fc7effaf2e7d1d
e_saito_bizright <e_saito@bizright.jp>
2021-02-19 15:07:05 +0900

    no message

--- bh_pkg_mgr.py
+++ bh_pkg_mgr.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 
+import subprocess
 import os
 
 PKG_FILE = "/var/lib/dpkg/status"
 LIC = "/usr/share/doc/"
 
 pkg=0
+ver=0
 dpd=0
 lic=0
 hmp=0
 unknown=0
+
+LIC_LIMIT = 10
 
 def get_license(pkg_name):
   global lic
@@ -22,27 +26,36 @@ def get_license(pkg_name):
     print("Copyright:",LIC_FILE)
     f_copyright = open(LIC_FILE, 'r')
 
+    print('License: ',end='')
     for data in f_copyright:
       name = data.split(" ")
       #datalist = f_copyright.readlines()
       if "License:" == name[0]:
-        cnt=1
-        print(data.strip('License: \r \n'),end='')
+        print(data.strip('License: \r \n')," ",end='')
+        cnt+=1
       elif "Public License:" == name[0]:
-        cnt=1
-        print(data.strip('License: \r \n'),end='')
+        print(data.strip('License: \r \n')," ",end='')
+        cnt+=1
+
+      if "chromium-" in pkg_name:
+        break
+      if cnt > LIC_LIMIT:
+        break
 
     if cnt == 0:
       unknown += 1 
     else:
       lic += 1
 
+    print('\r')
+
     f_copyright.close()
 
 def make_list():
   global pkg
-  global dpd
   global lic
+  global ver
+  global dpd
   global unknown
   global hmp
   f_status = open(PKG_FILE, 'r')
@@ -59,10 +72,11 @@ def make_list():
       print(data.rstrip('\n'))
       pkg += 1
       pkg_name = data.split()
-      if "chromium-" in pkg_name[1]:
-        continue
       get_license(pkg_name[1])
       
+    elif "Version:" == name[0]:
+      print(data.rstrip('\n'))
+      ver += 1
     elif "Depends:" == name[0]:
       print(data.rstrip('\n'))
       dpd += 1
@@ -80,8 +94,14 @@ def make_list():
 make_list()
 
 print("\n")
+print("============= Distribution Specific ==========")
+result = subprocess.run(["lsb_release","-a"], stdout=subprocess.PIPE)
+print("OS version: ",result.stdout.decode("utf-8"),end='')
+result = subprocess.run(["uname","-r"], stdout=subprocess.PIPE)
+print("OS version: ",result.stdout.decode("utf-8"),end='')
 print("============= Pakeging License Summary ===========")
 print("Package: ",pkg)
+print("Version: ",ver)
 print("Depends: ",dpd)
 print("License: ",lic)
 print("Unknown License: ",unknown)
