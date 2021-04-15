82a9a1ab234f348c4da7b23b320adf04a829cfd5
e_saito <e_saito@bizright.co.jp>
2021-04-15 17:58:43 +0900

    no message

--- bh_pkg_mgr.py
+++ bh_pkg_mgr.py
@@ -14,24 +14,28 @@ dpd=0
 lic=0
 hmp=0
 unknown=0
+n_param=0
 
 LIC_LIMIT = 10
+MAX_PARAM = 5
 
 def get_license(pkg_name):
   global lic
   global unknown
   global cpy
+  global n_param
 
   cnt=0
 
   LIC_FILE = LIC + pkg_name + "/copyright"
   if os.path.exists(LIC_FILE) == True:
-    if (len(sys.argv) > 1) and (sys.argv[1] == "copyright"):
-        print("Copyright:",LIC_FILE)
-        cpy+=1
+    print("Copyright:",LIC_FILE)
+    cpy+=1
+    n_param += 1
     f_copyright = open(LIC_FILE, 'r')
 
     print('License: ', end='')
+    n_param += 1
     for data in f_copyright:
       name = data.split(" ")
       if "License:" == name[0]:
@@ -58,35 +62,45 @@ def get_license(pkg_name):
     else:
       lic += 1
       print('\r')
-
-
     f_copyright.close()
+
+  else:
+    unknown += 1 
+    n_param += 2
+    print("Copyright: unknown\r")
+    print("License: unknown\r")
 
 def make_list():
   global pkg
   global lic
   global ver
   global dpd
-  global unknown
   global hmp
+  global n_param
+
   f_status = open(PKG_FILE, 'r')
 
   for data in f_status:
     name = data.split(" ")
 
     if "Package:" == name[0]:
+      if n_param > 0 & n_param < 5 :
+        print("Homepage: unknown\r")
+      n_param = 0
       print("-------------------------------------------------------------")
       print(data.rstrip('\n'))
       pkg += 1
+      n_param += 1
       pkg_name = data.split()
       get_license(pkg_name[1])
       
     elif "Version:" == name[0]:
       print(data.rstrip('\n'))
       ver += 1
-    elif "Depends:" == name[0]:
-      print(data.rstrip('\n'))
-      dpd += 1
+      n_param += 1
+  # elif "Depends:" == name[0]:
+  #   print(data.rstrip('\n'))
+  #   dpd += 1
   # elif "Description:" in data:
   #   print(data.rstrip('\n'))
   # elif "License" in data:
@@ -94,6 +108,7 @@ def make_list():
     elif "Homepage:" == name[0]:
       print(data.rstrip('\n'))
       hmp += 1
+      n_param += 1
 
   f_status.close()
 
@@ -101,11 +116,6 @@ def make_list():
 make_list()
 
 print("\n")
-if len(sys.argv) > 1:
-  arg_name = sys.argv[1]
-else:
-  arg_name =""
-print("command arg: ",len(sys.argv),arg_name)
 print("============= Distribution Specific ==========")
 result = subprocess.run(["lsb_release","-a"], stdout=subprocess.PIPE)
 print("OS version: ",result.stdout.decode("utf-8"), end='')
@@ -119,7 +129,6 @@ print("License: ",lic)
 print("Reliability: ",'{:.2%}'.format(lic/pkg))
 print("Unknown License: ",unknown)
 print("Homepage: ",hmp)
-if (len(sys.argv) > 1) and (sys.argv[1] == "copyright"):
-  print("Copyright:",cpy)
+print("Copyright:",cpy)
 print("==================================================")
 
